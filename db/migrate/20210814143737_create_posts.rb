47c94aee099ed30ce292b744b89365726dcf5a5f
atsushi oshima <aboutagial@yahoo.co.jp>
2021-08-15 10:58:22 +0900

    contentカラムを追加

--- db/migrate/20210814143737_create_posts.rb
+++ db/migrate/20210814143737_create_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
-      t.timestamps
+      
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
     end
   end
 end
