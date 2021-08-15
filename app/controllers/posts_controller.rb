a0296f0b832b6e85038855712a81b68fade4e166
atsushi oshima <aboutagial@yahoo.co.jp>
2021-08-15 22:22:23 +0900

    バリデーション・エラー件数作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  #before_action :set_post, only: [:]
 
   def index
     @posts = Post.all
@@ -9,8 +10,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
-     redirect_to new_post_path
+    @post = Post.new(post_params)
+    if @post.save
+     redirect_to new_post_path, notice: "新規メッセージを投稿しました"
+    else
+      render :new
+    end
   end
 
 end
@@ -18,4 +23,8 @@ end
 private
 def post_params
   params.require(:post).permit(:content)
+end
+
+def set_post
+  @post = Post.find(params[:id])
 end
